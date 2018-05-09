75e051dbe2a8d33e3e924f87fca5a920dfe9eff1
Ger-Jan te Dorsthorst <gerjantd@gmail.com>
2018-05-09 21:40:25 +0200

    Update module 04

--- 04/src/main/java/App.java
+++ 04/src/main/java/App.java
@@ -1,8 +1,11 @@
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 
 public class App {
   public static void main(String[] args) {
     Vertx vertx = Vertx.vertx();
-    System.out.println("Hello vertx object! "+vertx);
+    System.out.println("vertx: "+vertx);
+    Vertx vertx2 = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
+    System.out.println("vertx2: "+vertx2);
   }
 }
