d1e228d13f0aec8e9a2afcd1c59ff76864b00b1a
Ger-Jan te Dorsthorst <gerjantd@gmail.com>
2018-05-10 14:05:07 +0200

    Add module 10

---
+++ 10/src/main/java/io/vertx/starter/MainVerticle.java
@@ -0,0 +1,15 @@
+package io.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() {
+//    vertx.createHttpServer()
+//        .requestHandler(req -> req.response().end("Hello Vert.x!"))
+//        .listen(8080);
+    System.out.println("MainVerticle started");
+  }
+
+}
