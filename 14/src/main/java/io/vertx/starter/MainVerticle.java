bf724c573745165d5c8e57dfe7c4627328807a31
Ger-Jan te Dorsthorst <gerjantd@gmail.com>
2018-06-09 20:06:06 +0200

    Update module 14

--- 14/src/main/java/io/vertx/starter/MainVerticle.java
+++ 14/src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,76 +1,28 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.Handler;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private HttpServer httpServer = null;
+  private HttpClient httpClient = null;
 
   @Override
   public void start() throws Exception {
 
-    httpServer = vertx.createHttpServer();
+    httpClient = vertx.createHttpClient();
 
-    httpServer.requestHandler(new Handler<HttpServerRequest>() {
+    httpClient.getNow(80, "tutorials.jenkov.com", "/", new Handler<HttpClientResponse>() {
+
       @Override
-      public void handle(HttpServerRequest request) {
-
-        System.out.println("incoming request");
-
-        System.out.println("uri: "+request.uri());
-        System.out.println("path: "+request.path());
-        System.out.println("param p: "+request.getParam("p"));
-
-        Buffer fullRequestBody = Buffer.buffer();
-        if(request.method() == HttpMethod.POST){
-
-            request.handler(new Handler<Buffer>() {
-                @Override
-                public void handle(Buffer buffer) {
-                    fullRequestBody.appendBuffer(buffer);
-                    System.out.println("POST request handler: fullRequestBody: "+fullRequestBody);
-                }
-            });
-
-            request.endHandler(new Handler<Void>() {
-                @Override
-                public void handle(Void event) {
-                  // here you can access the 
-                  // fullRequestBody Buffer instance.
-                  System.out.println("POST request end handler: fullRequestBody: "+fullRequestBody);
-                }
-            });
-        }
-        HttpServerResponse response = request.response();
-        response.setStatusCode(200);
-        response.headers()
-          //.add("Content-Length", String.valueOf(57))
-          //.add("Content-Length", String.valueOf(8))
-          //.add("Content-Length", String.valueOf(16))
-          .add("Content-Length", String.valueOf("Vert.x is alive!".length()))
-          .add("Content-Type", "text/html");
-        response.write("Vert.x is alive!");
-        response.end();
-        System.out.println("response sent: "+response);
-        // alternatively:
-        //response.end("Vert.x is alive!");
-
+      public void handle(HttpClientResponse response) {
+        System.out.println("response received");
       }
+
     });
 
-    httpServer.listen(8080);
-       
-  }
-
-  @Override
-  public void stop() throws Exception {
-    httpServer.close();
   }
 
 }
