655ec268b295ecde678ab9d9a9d6ecad64dd1910
Ger-Jan te Dorsthorst <gerjantd@gmail.com>
2018-11-25 11:43:43 +0100

    Commit changes unfinished module 14

--- 14/src/main/java/io/vertx/starter/MainVerticle.java
+++ 14/src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,6 +1,7 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.Handler;
@@ -14,11 +15,23 @@ public class MainVerticle extends AbstractVerticle {
 
     httpClient = vertx.createHttpClient();
 
-    httpClient.getNow(80, "tutorials.jenkov.com", "/", new Handler<HttpClientResponse>() {
+    //httpClient.getNow(80, "tutorials.jenkov.com", "/", new Handler<HttpClientResponse>() {
+    httpClient.getNow("jsonplaceholder.typicode.com/posts", "/1", new Handler<HttpClientResponse>() {
 
       @Override
       public void handle(HttpClientResponse response) {
         System.out.println("response received");
+
+        response.bodyHandler(new Handler<Buffer>() {
+
+          @Override
+          public void handle(Buffer buffer) {
+            System.out.println("response ("+buffer.length()+"): ");
+            //System.out.println(buffer.getString(0, buffer.length()));
+          }
+
+        });
+
       }
 
     });
