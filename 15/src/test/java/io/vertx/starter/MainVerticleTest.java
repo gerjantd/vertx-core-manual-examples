0f373af10b859e47001150f65c6c306160a6f7e9
Ger-Jan te Dorsthorst <gerjantd@gmail.com>
2018-11-25 13:19:48 +0100

    Add module 15

---
+++ 15/src/test/java/io/vertx/starter/MainVerticleTest.java
@@ -0,0 +1,35 @@
+package io.vertx.starter;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class MainVerticleTest {
+
+  private Vertx vertx;
+
+  @Before
+  public void setUp(TestContext tc) {
+    vertx = Vertx.vertx();
+    vertx.deployVerticle(MainVerticle.class.getName(), tc.asyncAssertSuccess());
+  }
+
+  @After
+  public void tearDown(TestContext tc) {
+    vertx.close(tc.asyncAssertSuccess());
+  }
+
+  @Test
+  public void testNothing(TestContext tc) {
+    Async async = tc.async();
+    tc.assertTrue(true);
+    async.complete(); 
+  }
+
+}
