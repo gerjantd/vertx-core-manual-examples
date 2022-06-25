7a0832a32a943aac96eff577a32fb48fba334e2c
Ger-Jan te Dorsthorst <gerjantd@gmail.com>
2022-06-25 16:37:49 +0200

    Rework

--- 00/src/test/java/io/vertx/starter/MainVerticleTest.java
+++ 00/src/test/java/io/vertx/starter/MainVerticleTest.java
@@ -1,6 +1,30 @@
 package io.vertx.starter;
 
 import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+public class TestMainVerticle {
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    testContext.completeNow();
+  }
+}
+
+/*
+package io.vertx.starter;
+
+import io.vertx.core.Vertx;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -38,3 +62,4 @@ public class MainVerticleTest {
   }
 
 }
+*/
